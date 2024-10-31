5762af6b59c0ad9b9ef4c56350e19d28a0211991
Kata <kata.niva03@gmail.com>
2024-10-31 08:26:51 +0200

    H1 valmis

--- H1/main.cpp
+++ H1/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 void calSum(int a, int b) {
     int sum = a + b;
-    cout << "Lukujen summa on: " << sum << "\n";
+    cout << "Lukujen summa on: " << sum << endl;
 }
 
 void calDiv(float a, float b) {
     if (b == 0) {
-        cout << "Virhe: Jakaja ei voi olla nolla!\n";
+        cout << "Virhe: Jakaja ei voi olla nolla" << endl;
     } else {
         float div = a / b;
-        cout << "Lukujen osamaara on: " << div << "\n";
+        cout << "Lukujen osamaara on: " << div << endl;;
     }
 }
 
@@ -23,7 +23,7 @@ int retSum(int a, int b) {
 
 float retDiv(float a, float b) {
     if (b == 0) {
-        cout << "Virhe: Jakaja ei voi olla nolla!\n";
+        cout << "Virhe: Jakaja ei voi olla nolla!" << endl;
         return 0.0f;
     }
     return a / b;
@@ -45,11 +45,11 @@ int main()
     calDiv(a, b);
 
     int summa = retSum(a, b);
-    cout << "Palautettu summa: " << summa << "\n";
+    cout << "Palautettu summa: " << summa << endl;
 
     float osamaara = retDiv(a, b);
     if (b != 0) {
-        cout << "Palautettu osamaara: " << osamaara << "\n";
+        cout << "Palautettu osamaara: " << osamaara << endl;
         return 0.0f;
     }
 }
--- H2/main.cpp
+++ H2/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Sup guys!" << endl;
     return 0;
 }
