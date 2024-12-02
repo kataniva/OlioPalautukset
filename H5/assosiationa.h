ae95d2177819a54e045a10748ac06259427731a6
Kata <kata.niva03@gmail.com>
2024-12-02 10:17:13 +0200

    H5 valmis

---
+++ H5/assosiationa.h
@@ -0,0 +1,14 @@
+#ifndef ASSOSIATIONA_H
+#define ASSOSIATIONA_H
+
+#include "classb.h"
+class AssosiationA
+{
+private:
+    ClassB objectB;
+public:
+    AssosiationA(ClassB);
+    string getBinfo();
+    void setBinfo(string);
+};
+#endif // ASSOSIATIONA_H
---
+++ H5/classb.cpp
@@ -0,0 +1,16 @@
+#include "classb.h"
+
+string ClassB::getInfo() const
+{
+    return info;
+}
+
+void ClassB::setInfo(const string &newInfo)
+{
+    info = newInfo;
+}
+
+ClassB::ClassB()
+{
+
+}
